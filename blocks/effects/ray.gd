5bafc7ce2a6418fe3982f95dfdf89dc09c25f2a5
Andres David Calderon <andresdavidcalderonjimenez@gmail.com>
2026-01-22 21:26:00 -0500

    shoot ray

--- blocks/effects/ray.gd
+++ blocks/effects/ray.gd
@@ -3,5 +3,13 @@ extends Node3D
 var to
 var from
 
-func _ready() -> void:
+func shoot(from:Vector3,to:Vector3):
+	$Path3D.curve=Curve3D.new()
+	$Path3D.curve.add_point(from)
+	$Path3D.curve.add_point(to)
+	await get_tree().create_timer(0.2).timeout
+	queue_free()
+
+func _process(delta: float) -> void:
 	global_position=Vector3.ZERO
+	global_rotation=Vector3.ZERO
