e9aec372d51e68c6d336a108291bf8d5d18ec83b
Andres David Calderon <andresdavidcalderonjimenez@gmail.com>
2026-01-24 17:35:57 -0500

    work on flipper redesign

--- blocks/flipper.gd
+++ blocks/flipper.gd
@@ -7,12 +7,13 @@ extends Node3D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Interactive.tried_to_pick.connect(on_tried_to_pick)
+	$FlipperBody.add_collision_exception_with($FlipperHolder)
+	$FlipperHolder.add_collision_exception_with($FlipperBody)
 
 func _physics_process(delta: float) -> void:
 	if bumped:
-		$FlipperBody.apply_force(Vector3.UP*force*force_multiplier,$ForceCenter.position)
+		$FlipperBody.apply_force(Vector3.UP*force*force_multiplier,$FlipperBody/ForceCenter.position)
 
 func on_tried_to_pick(intersection):
 	if (not intersection.is_empty()) and intersection["collider"]==$FlipperBody:
 		$AnimationPlayer.play("bump")
-		print("Ckick")
--- progress.gd
+++ progress.gd
@@ -4,11 +4,14 @@ signal won
 
 var levels=[
 	"res://levels/level1.tscn",
-	"res://levels/level2.tscn"
+	"res://levels/level2.tscn",
+	"res://levels/level3.tscn"
 ]
 var current_level=0
 
-func set_level(new_level=current_level+1):
+func set_level(new_level=-1):
+	if new_level==-1:
+		new_level=current_level+1
 	current_level=new_level
 	if current_level<levels.size():
 		get_tree().change_scene_to_file(levels[current_level])
