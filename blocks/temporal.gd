fa52c3892d4bd5d5525f14763323a63c3bbcfcc0
Andres David Calderon <andresdavidcalderonjimenez@gmail.com>
2026-01-25 15:59:05 -0500

    More design for level3

---
+++ blocks/temporal.gd
@@ -0,0 +1,27 @@
+@tool
+extends CSGBox3D
+
+@export var on_material:StandardMaterial3D
+@export var off_material:StandardMaterial3D
+
+@export var on=true:
+	set=set_on
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	Interactive.tried_to_pick.connect(on_tried_to_pick)
+	
+
+func set_on(new_on):
+	on=new_on
+	if on:
+		collision_layer=5
+		collision_mask=5
+		material=on_material
+	else:
+		collision_layer=7
+		collision_mask=7
+		material=off_material
+func on_tried_to_pick(intersection):
+	if (not intersection.is_empty()) and intersection["collider"]==self:
+		on=not on
